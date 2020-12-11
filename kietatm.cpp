7711a1baf557c0c3b4be1bdbdaf4823100d136d6
Crappyraj <67316208+Crappyraj@users.noreply.github.com>
2020-12-11 18:03:36 +0530

    Add files via upload

---
+++ kietatm.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a,b,c;
+    int t=0;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    t=a*100+b*500+c*2000;
+    cout<<t<<endl;
+    
+    return 0;
+}
